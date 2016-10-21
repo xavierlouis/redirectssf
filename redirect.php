a5a02c27cb89a23c364abd97b27eb5339864ac72
Xavier <xavier.gouin@bluescale.com>
2016-10-21 15:33:00 +0200

    url updated

--- redirect.php
+++ redirect.php
@@ -1,10 +1,10 @@
 <script>
         $(document).ready(function (){
          if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
-             window.location.href = 'https://play.google.com/store/apps/details?id=com.youcarenow.ssf';
+             window.location.href = 'https://play.google.com/store/apps/details?id=com.youcarenow.hatzalah';
          }
          if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
-             window.location.href = 'https://itunes.apple.com/app/ssf-sauveteurs-sans-frontieres/id1090891296?ls=1&mt=8';
+             window.location.href = 'https://itunes.apple.com/us/app/sos-hatzalah/id1155489309?ls=1&mt=8';
          }
         });
 </script>
